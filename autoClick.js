4ccffbaf4c418cf0a02aba2995f0230d24d80732
trippybruh <104927119+trippybruh@users.noreply.github.com>
2024-09-30 01:22:14 +0200

    Add files via upload

--- autoClick.js
+++ autoClick.js
@@ -2,7 +2,7 @@ const cloudscraper = require('cloudscraper');
 const axios = require('axios');
 
 const startTime = Date.now();
-const DELAY = 310; // ms
+const DELAY = 400; // ms
 const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI3NjI3MzY0LCJleHAiOjE3Mjc3MTM3NjQsInR5cGUiOiJhY2Nlc3MifQ.kJ__EfYByp9jxeWngzc3jZpfAZ5UHkqMIy2aYu4OEQc';
 const bearerTokens = [
     //bearerPrefix.concat('E8YZmA0aBznfiOF92H3OJxZqCIWqX_fW_dxwTvSSoh0')
