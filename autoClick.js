796125bc978ea53010c6455ab52af1d3ed185c25
trippybruh <104927119+trippybruh@users.noreply.github.com>
2024-10-04 19:50:38 +0200

    Update autoClick.js

--- autoClick.js
+++ autoClick.js
@@ -6,7 +6,7 @@ const app = express();
 const startTime = Date.now();
 const REQ_INTERVAL_DELAY = 330; // ms
 const INTRA_REQ_DELAY = 300;
-const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI3OTc3NTEwLCJleHAiOjE3MjgwNjM5MTAsInR5cGUiOiJhY2Nlc3MifQ.7hSI75CL0ta7iFwTY23-nkE1eLNlfEZ0aMe3TP8YhQQ';
+const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI4MDY0MTkxLCJleHAiOjE3MjgxNTA1OTEsInR5cGUiOiJhY2Nlc3MifQ.WvCKdoUupAYZRotjwnP1_8PGGVevgBbQVb7MMY-B7Bs';
 const bearerTokens = [
     //bearerPrefix.concat('E8YZmA0aBznfiOF92H3OJxZqCIWqX_fW_dxwTvSSoh0')
     bearer
