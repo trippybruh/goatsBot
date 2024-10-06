c0c468062b8422671fbf7115dc02550394f9319d
Niccolò <meinicco@gmail.com>
2024-10-07 01:36:43 +0200

    .

--- flipperSim.js
+++ flipperSim.js
@@ -1,3 +1,5 @@
+const Chance = require('chance');
+const chance = new Chance();
 const startTime = Date.now();
 const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI3ODkwMTU1LCJleHAiOjE3Mjc5NzY1NTUsInR5cGUiOiJhY2Nlc3MifQ.OIvv3TonWy7xl-j3mkicIycMHT1Z2RZWV1CvIhA5daQ';
 const bearerTokens = [
@@ -7,9 +9,9 @@ const bearerTokens = [
 const REQ_INTERVAL_DELAY = 330; // ms
 
 // head or tail game config
-const totalBets = 10000;
-const betAmount = 50000;
-const expectedVolume = totalBets * betAmount * 1.5;
+const totalBets = 50000;
+const betAmount = 2000;
+const expectedVolume = totalBets * betAmount * 1.5; // non torna in app
 const expectedTimeRequired = totalBets * (REQ_INTERVAL_DELAY/1000);
 let head_tail = "HEADS";
 
@@ -45,7 +47,7 @@ function printStatistics() {
     console.log(`Tempo dall'avvio: ${elapsedTime} secondi (${(elapsedTimeMin).toFixed(0)} minuti) --- Al completamento: ${(expectedTimeRequired - elapsedTime).toFixed(0)} secondi --- Target richieste/min: ${60000/REQ_INTERVAL_DELAY})
         Vincite totali: ${winCount} --- Sconfitte totali: ${lossCount} --- Win rate attuale: ${winRate} % --- Guadagno/perdita: ${netChange} GOATS
         Serie più lunga di vittorie: ${winStreak} --- Serie più lunga di sconfitte: ${lossStreak} --- Max upside: ${netMaxUpside} --- Max downside: ${netMaxDownside} 
-        Volume effettivo generato: ${currentVolume} GOATS (target: ${expectedVolume} -> ${(currentVolume/expectedVolume) * 100} % completato)`);
+        Volume effettivo generato: ${currentVolume} GOATS (target: ${expectedVolume} -> ${((currentVolume/expectedVolume) * 100).toFixed(2)} % completato)`);
 }
 
 function testLogStats(result) {
@@ -84,36 +86,33 @@ function testLogStats(result) {
 }
 
 function testRandomResult() {
-    return Math.random() < 0.5;
+    return chance.bool();
 }
 
-async function testRequestCycle() {
-    const consoleLogStep = 30;
+async function simulateReqCycle() {
     let cycles = 0;
-    const data = {
-        "head_tail": head_tail,
-        "bet_amount": betAmount
-    };
-    setInterval(async () => {
-        if (cycles === totalBets) {
-            printStatistics();
-            await sleep(1000);
-            process.exit(0);
-        }
+    const consoleLogStep = (totalBets/10).toFixed(0);
+    while (cycles < totalBets) {
         testLogStats(testRandomResult());
-         if (cycles % consoleLogStep === 0) {
-            printStatistics();
-        }
-        cycles++;
-    }, REQ_INTERVAL_DELAY);
+        if (cycles % consoleLogStep === 0) {
+           printStatistics();
+       }
+       cycles++;
+    }
+    printStatistics();
+    await sleep(1000);
+    process.exit(0);
 }
 
 function testStart() {
     bearerTokens.forEach(async (bearerToken) => {
-        console.log(`Inizio cicli di flipping per Bearer Token: ${bearer.slice(0, 5)}...${bearer.slice(-5)} --- TEST FUNCTION WITH GAME SIMULATOR
+        console.log(`Inizio cicli di flipping per Bearer Token: ${bearerToken.slice(0, 5)}...${bearer.slice(-5)} --- TEST FUNCTION WITH GAME SIMULATOR
         Configurato per eseguire max ${(1000 / REQ_INTERVAL_DELAY).toFixed(2)} richieste/s --- Max richieste (partite): ${totalBets}
         Importo per partita: ${betAmount} GOATS --- Volume atteso: ${(expectedVolume)} GOATS
         Tempo stimato per eseguire tutte le richieste: ${expectedTimeRequired} secondi (${(expectedTimeRequired / 60).toFixed(0)} minuti)`);
-        await testRequestCycle();
+        await  simulateReqCycle();
     });
 }
+
+//main
+testStart();
