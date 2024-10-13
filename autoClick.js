fd96ce05eea386e412807a2977b595a6730a682a
Niccolò <meinicco@gmail.com>
2024-10-13 18:29:53 +0200

    ...

--- autoClick.js
+++ autoClick.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const startTime = Date.now();
-const REQ_INTERVAL_DELAY = 700; // ms
+const REQ_INTERVAL_DELAY = 760; // ms
 const INTRA_REQ_DELAY = 650;
 const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNzI1NWM3NDU0ZmY1MGRmYjhjZjM0IiwiaWF0IjoxNzI4Nzg0MDI0LCJleHAiOjE3Mjg4NzA0MjQsInR5cGUiOiJhY2Nlc3MifQ.8G8hdvOuX2ZRTEdOE9CmaH7Ap4VEfflw0D9zzoELKHc';
 const bearerTokens = [
