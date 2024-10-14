203b5461c0c3ec97deadc17728a414a25c15654f
Niccolò <meinicco@gmail.com>
2024-10-15 00:57:05 +0200

    .

--- autoClick.js
+++ autoClick.js
@@ -5,7 +5,7 @@ const app = express();
 const startTime = Date.now();
 const REQ_INTERVAL_DELAY = 330; // ms
 const INTRA_REQ_DELAY = 300;
-const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI4ODY5MjU3LCJleHAiOjE3Mjg5NTU2NTcsInR5cGUiOiJhY2Nlc3MifQ._37BLjbZq6yCdqFpScILGP9PYGry4xuNfv9QzfMG5o4';
+const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNzIxOTkwMTRmNDcwZTVhMDUxNjcxIiwiaWF0IjoxNzI4OTQ2MDU5LCJleHAiOjE3MjkwMzI0NTksInR5cGUiOiJhY2Nlc3MifQ.2mT7GFcGNNbD1bdD2g774SZkWGmSjBcQnWJTCq8ggjU';
 const bearerTokens = [
     bearer
 ];
