2325a9ee199957c1780dc25edd44b118e6b23e78
Niccolò <meinicco@gmail.com>
2024-10-15 06:01:22 +0200

    .

--- autoStupidBet.js
+++ autoStupidBet.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const startTime = Date.now();
-const REQ_INTERVAL_DELAY = 3000000; // ms
+const REQ_INTERVAL_DELAY = 2000000; // ms
 const INTRA_REQ_DELAY = 1000;
 const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNzIxOTkwMTRmNDcwZTVhMDUxNjcxIiwiaWF0IjoxNzI4OTQ2MDU5LCJleHAiOjE3MjkwMzI0NTksInR5cGUiOiJhY2Nlc3MifQ.2mT7GFcGNNbD1bdD2g774SZkWGmSjBcQnWJTCq8ggjU';
 const bearerTokens = [
