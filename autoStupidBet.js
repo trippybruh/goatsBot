ab7ef24f3fc02e3212467dbadc340421cc08fe72
Niccolò <meinicco@gmail.com>
2024-10-18 01:39:57 +0200

    .

--- autoStupidBet.js
+++ autoStupidBet.js
@@ -3,17 +3,19 @@ const express = require('express');
 
 const app = express();
 const startTime = Date.now();
-const REQ_INTERVAL_DELAY = 2000000; // ms
-const INTRA_REQ_DELAY = 1000;
+const REQ_INTERVAL_DELAY = 30000; // ms
+const INTRA_REQ_DELAY = 5000;
 const bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNzIxOTkwMTRmNDcwZTVhMDUxNjcxIiwiaWF0IjoxNzI4OTQ2MDU5LCJleHAiOjE3MjkwMzI0NTksInR5cGUiOiJhY2Nlc3MifQ.2mT7GFcGNNbD1bdD2g774SZkWGmSjBcQnWJTCq8ggjU';
 const bearerTokens = [
     bearer
 ];
 
 const winChanceMilestone = 100;
-const bet_amount = 999949;
+const bet_amount = 999949; // G
+const target_volume = 20000000; // G
 let successCount = 0;
 let failureCount = 0;
+let volume;
 
 function getElapsedTimeInSeconds() {
     return ((Date.now() - startTime) / 1000).toFixed(2);
@@ -44,10 +46,9 @@ async function makeRequest(bearerToken) {
         successCount++;
         return jsonResponse;
     } catch (error) {
-        console.log(`Errore richiesta: ${(error.message).slice(0, 5)} - Ritento tra un minuto`)
+        console.log(`Errore richiesta bet: ${(error.message).slice(0, 5)}`)
         failureCount++;
-        await sleep(66000);
-        return makeRequest(bearerToken);
+        return null;
     }
 }
 
@@ -58,31 +59,44 @@ async function sleep(ms) {
 function logStatistics() {
     const elapsedTime = getElapsedTimeInSeconds();
     const elapsedTimeMin = elapsedTime/60;
-    const volume = bet_amount*successCount*(winChanceMilestone/100);
+
+    volume = (bet_amount-(Math.floor(bet_amount*0.01)))*successCount;
     const gained = (-0.01*bet_amount*successCount);
     let ratioWL = 'inf';
     if (failureCount !== 0) {
         ratioWL = (successCount/failureCount).toFixed(4);
     }
     console.log(`Tempo dall'avvio: ${Math.floor(elapsedTime/3600)} ore ${((elapsedTime/60) % 60).toFixed(0)} minuti ${(elapsedTime % 60).toFixed(0)} secondi`);
-    console.log(`-> Richieste elaborate: ${successCount} --- Richieste fallite: ${failureCount} `);
-    console.log(`-> Successi/Fallimenti: ${ratioWL} --- Richieste totali/ora: ${((successCount + failureCount)/(elapsedTimeMin*60)).toFixed(2)} (target: ${((60000*60)/REQ_INTERVAL_DELAY).toFixed(1)}/h)`);
-    console.log(`-> Guadagno (stupid bet): ${Math.round(gained)} GOATS --- Volume: ${Math.round(volume)} GOATS`);
+    console.log(`-> Richieste elaborate: ${successCount} --- Richieste fallite: ${failureCount} --- Richieste ancora necessarie: ${Math.ceil((target_volume-volume)/(volume/successCount))}`);
+    console.log(`-> Successi/Fallimenti: ${ratioWL} --- Richieste totali/min: ${((successCount + failureCount)/(elapsedTimeMin)).toFixed(2)} (target: ${((60000)/REQ_INTERVAL_DELAY).toFixed(1)}/min)`);
+    console.log(`-> Perdite totali (stupid bet): ${Math.round(gained)} GOATS --- Volume generato per bet: ${volume/successCount} GOATS )`);
+    console.log(`-> Volume totale: ${volume} GOATS (${((volume/target_volume)*100).toFixed(1)}% del volume target) --- Tempo stimato al target: ${((REQ_INTERVAL_DELAY/1000)*Math.ceil((target_volume-volume)/(volume/successCount))).toFixed(1)} secondi`);
 }
 
 async function performRequestCycle(bearerToken) {
     await makeRequest(bearerToken);
     logStatistics();
-    setInterval(async () => {
+    let consoleSteps = [3, 5, 7];
+    let cycles;
+    const intervalId = setInterval(async () => {
         await makeRequest(bearerToken);
-        logStatistics();
+        if (cycles % consoleSteps[Math.floor(Math.random() * consoleSteps.length)] === 0) {
+            logStatistics();
+        }
+        if (volume >= target_volume) {
+            console.log(`Bet stupide eseguite :) -> CAPRE MUNTE: ${volume} GOATS`);
+            await sleep(3000);
+            clearInterval(intervalId);
+            process.exit(0);
+        }
     }, REQ_INTERVAL_DELAY)
+    process.exit(0);
 }
 
 function start() {
     bearerTokens.forEach(async (bearerToken) => {
         console.log(`Inizio cicli di richieste per Bearer Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}
-        Configurato per eseguire max ${((60000*60)/REQ_INTERVAL_DELAY).toFixed(2)} richieste/orarie --- Timeout minimo tra una richiesta ed un altra: ${((60000*60)/REQ_INTERVAL_DELAY/1000).toFixed(3)} ora`);
+        Configurato per eseguire max ${((60000*60)/REQ_INTERVAL_DELAY).toFixed(2)} richieste/min --- Timeout minimo tra una richiesta ed un altra: ${((60000)/REQ_INTERVAL_DELAY/1000).toFixed(3)} min`);
         await performRequestCycle(bearerToken);
     });
 }
