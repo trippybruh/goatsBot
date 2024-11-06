ff4e2225d25eea826121c07887ae7ed1926902a9
Niccolò <meinicco@gmail.com>
2024-11-07 00:11:01 +0100

    .

---
+++ auto_script.js
@@ -0,0 +1,331 @@
+const cloudscraper = require('cloudscraper');
+
+const get_mission_api_url = 'https://api-mission.goatsbot.xyz/missions/user';
+const get_checkin_api_url = 'https://api-checkin.goatsbot.xyz/checkin/user';
+const get_refresh_api_url = 'https://dev-api.goatsbot.xyz/auth/refresh-tokens'
+const execute_mission_api_url = 'https://dev-api.goatsbot.xyz/missions/action/'
+const execute_checkin_api_url = 'https://api-checkin.goatsbot.xyz/checkin/action/'
+const execute_cinema_api_url = 'https://dev-api.goatsbot.xyz/goat-cinema/watch'
+const execute_bet_balance_api_url = 'https://api-dice.goatsbot.xyz/dice/action'
+
+const headerApi = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/129.0.0.0 Safari/537.36',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Origin': 'https://dev.goatsbot.xyz',
+    'Referer': 'https://dev.goatsbot.xyz/',
+    'Sec-CH-UA': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    'Sec-CH-UA-Mobile': '?0',
+    'Sec-CH-UA-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+
+}
+
+const userTokens = [
+    {
+        bearer: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZDQwNDNmZjM0YmE2YzMyNWMzNjlhIiwiaWF0IjoxNzMwODQyMDQ5LCJleHAiOjE3MzA5Mjg0NDksInR5cGUiOiJhY2Nlc3MifQ.fjn5cSBBh8MBH_qXR8l_9g42_bd2lIOtbY33kE8wdWE',
+        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZDQwNDNmZjM0YmE2YzMyNWMzNjlhIiwiaWF0IjoxNzMwODQyMDQ5LCJleHAiOjE3MzM0MzQwNDksInR5cGUiOiJyZWZyZXNoIn0.ttA-glV8Skp3A6eGtwfCK17hos5D4xkc9w5t9hiIsUA'
+    },
+    {
+        bearer: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZDM5ZDBmZjM0YmE2YzMyNGVmYjY0IiwiaWF0IjoxNzMwODQyMTQ5LCJleHAiOjE3MzA5Mjg1NDksInR5cGUiOiJhY2Nlc3MifQ.3SqEdfjxWQ7ks27dHjUzVR4YPFXBoAlfDzraTr3N0vw',
+        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZlZDM5ZDBmZjM0YmE2YzMyNGVmYjY0IiwiaWF0IjoxNzMwODQyMTQ5LCJleHAiOjE3MzM0MzQxNDksInR5cGUiOiJyZWZyZXNoIn0.4BEpMaSItLNKSkZ2xbyNIhzd67aZMkQ6BsmniYAsrmw'
+    },
+]
+
+const adsMissionId = '66db47e2ff88e4527783327e'
+
+let successCount = 0
+let errorCount = 0
+let totalBalance = 0;
+
+//START
+
+loop().then()
+
+async function loop() {
+    while (true) {
+        await start()
+        console.log(`📊 Balance: ${totalBalance} | Avg Balance: ${totalBalance / userTokens.length} | Total Users: ${userTokens.length}`)
+        totalBalance = 0
+        await pauseExecution(1000)
+    }
+}
+
+async function start() {
+    console.log('🚀 Start')
+    for (const token of userTokens) {
+        const {bearer, refresh} = token
+        await missions(bearer, refresh) // Ads compresa
+        await checkin(bearer, refresh)
+        await cinema(bearer, refresh)
+        await sumBalance(bearer, refresh) // senza bet disonesta
+        console.log(`📊 Token: ${bearer.slice(-5)} | Success: ${successCount} | Error: ${errorCount}`)
+        await pauseExecution(500)
+    }
+}
+
+//USER
+
+async function sumBalance(bearer, refresh) {
+    const balance = await getBalance(bearer, refresh)
+    totalBalance += balance
+}
+
+async function getBalance(bearer, refresh) {
+    const data = {
+        point_milestone: 90,
+        is_upper: false,
+        bet_amount: 5
+    }
+
+    const options = {
+        method: 'POST',
+        url: execute_bet_balance_api_url,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`,
+        },
+        body: JSON.stringify(data),
+        timeout: 5000
+    }
+
+    try{
+        const response = await cloudscraper(options)
+        const jsonResponse = JSON.parse(response)
+
+        return jsonResponse?.user?.balance || 0
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await getBalance(bearerToken, refreshToken)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+    }
+}
+
+async function getRefreshToken(refresh, bearer) {
+    const data = {refreshToken: refresh}
+    const options = {
+        method: 'POST',
+        url: get_refresh_api_url,
+        headers: {
+            ...headerApi
+        },
+        body: JSON.stringify(data),
+
+    };
+
+    try {
+        const response = await cloudscraper(options);
+        const jsonResponse = JSON.parse(response);
+
+        const refreshToken = jsonResponse.tokens.refresh.token
+        const bearerToken = jsonResponse.tokens.access.token
+
+        const index = userTokens.findIndex((element) => element.bearer === bearer)
+        userTokens[index].bearer = bearerToken
+        userTokens[index].refresh = refreshToken
+
+        return {refreshToken, bearerToken}
+    } catch (error) {
+        logError(error.message, bearer);
+        return null;
+    }
+}
+
+//MISSIONS
+
+async function missions(bearer, refresh) {
+    const missionsData = await getMissions(bearer, refresh)
+
+    if (missionsData) {
+        for (const [missionGroup, missions] of Object.entries(missionsData)) {
+            for (const mission of missions) {
+                if (mission.status === false) {
+                    await executeMission(bearer, refresh, mission._id);
+                }
+            }
+        }
+        console.log(`📊 Completed Missions`);
+    }
+}
+
+async function executeMission(bearer, refresh, missionId) {
+    const options = {
+        method: 'POST',
+        url: `${execute_mission_api_url}${missionId}`,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`,
+        },
+    };
+
+    try {
+        const response = await cloudscraper(options);
+        if (missionId === adsMissionId)
+            successCount++
+        console.log(`📊 Completed Mission: ${missionId}`);
+        return JSON.parse(response);
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await executeMission(bearerToken, refreshToken, missionId)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+    }
+}
+
+async function getMissions(bearer, refresh) {
+    const options = {
+        method: 'GET',
+        url: get_mission_api_url,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`
+        }
+    }
+
+    try {
+        const response = await cloudscraper(options);
+        return JSON.parse(response);
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await getMissions(bearerToken, refreshToken)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+        return null;
+    }
+}
+
+//CHECKIN
+
+async function checkin(bearer, refresh) {
+    const checkinData = await getCheckin(bearer, refresh)
+
+    if (checkinData) {
+        for (const checkin of checkinData.result) {
+            if (checkin.status === false) {
+                await executeCheckin(bearer, refresh, checkin._id);
+                break
+            }
+        }
+        console.log(`📊 Completed Checkin`);
+    }
+
+}
+
+async function executeCheckin(bearer, refresh, checkinId) {
+    const options = {
+        method: 'POST',
+        url: `${execute_checkin_api_url}${checkinId}`,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`,
+        }
+    }
+
+    try {
+        const response = await cloudscraper(options);
+        console.log(`📊 Completed Checkin: ${checkinId}`);
+        return JSON.parse(response);
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await executeCheckin(bearerToken, refreshToken, checkinId)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+    }
+
+
+}
+
+async function getCheckin(bearer, refresh) {
+    const options = {
+        method: 'GET',
+        url: get_checkin_api_url,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`
+        }
+    }
+
+    try {
+        const response = await cloudscraper(options);
+        return JSON.parse(response);
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await getCheckin(bearerToken, refreshToken)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+        return null;
+    }
+}
+
+//CINEMA
+
+async function cinema(bearer, refresh) {
+    for (let i = 0; i < 3; i++) {
+        await executeCinema(bearer, refresh)
+    }
+    console.log(`📊 Completed Cinema`);
+}
+
+async function executeCinema(bearer, refresh) {
+    const options = {
+        method: 'POST',
+        url: execute_cinema_api_url,
+        headers: {
+            ...headerApi,
+            Authorization: `Bearer ${bearer}`,
+        }
+    }
+
+    try {
+        const response = await cloudscraper(options);
+        return JSON.parse(response);
+    } catch (error) {
+        if (error.statusCode === 401) {
+            const {refreshToken, bearerToken} = await getRefreshToken(refresh, bearer)
+            if (refreshToken) {
+                return await executeCinema(bearerToken, refreshToken)
+            }
+        } else {
+            logError(error.message, bearer);
+        }
+    }
+}
+
+//UTILS
+
+function pauseExecution(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function logError(msg, bearerToken) {
+    errorCount++
+    console.log(`📊 ${msg} | Token: ${bearerToken.slice(-5)} `);
+    return null;
+}
--- missionChecker.js
+++ missionChecker.js
@@ -2,20 +2,117 @@ const cloudscraper = require('cloudscraper');
 const express = require('express');
 const app = express();
 const startTime = Date.now();
-const myBearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzI4MzQ2NzE0LCJleHAiOjE3Mjg0MzMxMTQsInR5cGUiOiJhY2Nlc3MifQ.ffrpLDegsogtLnJBpiuYB2RATmNet2S1AZPvntXe2jg';
+
+const myBearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjZmMDI2NGZhNzVkYjBjZjYzYmY4YjAwIiwiaWF0IjoxNzMwOTMyMjAwLCJleHAiOjE3MzEwMTg2MDAsInR5cGUiOiJhY2Nlc3MifQ.ori1iYMpbCsYdbjbLfpz9J9jjBeMljHLcd3bTGlqlsk';
 const bearerTokens = [
     myBearer,
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNDBhNjYzOGE4ZTVkMjY0YTk2Mjg2IiwiaWF0IjoxNzI4NDI5NzM1LCJleHAiOjE3Mjg1MTYxMzUsInR5cGUiOiJhY2Nlc3MifQ.UWmiNeMFy6ClSxVia10m7x0pY7TbV1fRedwSkVDyj7A',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcwNDc5YTMwMTRmNDcwZTVhYjViZDdlIiwiaWF0IjoxNzI4NDMzNjg0LCJleHAiOjE3Mjg1MjAwODQsInR5cGUiOiJhY2Nlc3MifQ.UTft22gZ1TCHmp0xcr5iZrw4rO1Ok35eLnJfXEizJ1E'
-];
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcxMjYyMzFmMDNmYTFmNjhhYjcyZjhmIiwiaWF0IjoxNzMwOTMyMzY4LCJleHAiOjE3MzEwMTg3NjgsInR5cGUiOiJhY2Nlc3MifQ.8i4TvFPlhy-fCxkO6rkxNoYGIls-n-CaOKXYP82PEAI',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjcxM2FiNDgzNmVmODEzMWM1MjAyNmE2IiwiaWF0IjoxNzMwOTMyNDI3LCJleHAiOjE3MzEwMTg4MjcsInR5cGUiOiJhY2Nlc3MifQ.NSv8ub-bNPd9t4Qnjr7XMWD-McGA0lAFzJ0eLtEZaFI',
+]
+
+const betData = {
+    "point_milestone": 90,
+    "is_upper": false,
+    "bet_amount": 5
+};
+
+const headersApi = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Origin': 'https://dev.goatsbot.xyz',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Referer': 'https://dev.goatsbot.xyz/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-GPC': '1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+}
+
+const get_mission_api_url = 'https://api-mission.goatsbot.xyz/missions/user';
+const get_checkin_api_url = 'https://api-checkin.goatsbot.xyz/checkin/user';
+const get_refresh_api_url = 'https://dev-api.goatsbot.xyz/auth/refresh-tokens'
+const execute_mission_api_url = 'https://dev-api.goatsbot.xyz/missions/action/'
+const execute_checkin_api_url = 'https://api-checkin.goatsbot.xyz/checkin/action/'
+const execute_cinema_api_url = 'https://dev-api.goatsbot.xyz/goat-cinema/watch'
+const execute_bet_balance_api_url = 'https://api-dice.goatsbot.xyz/dice/action'
 
 let bigMissSuccess = 0;
 let successCount = 0;
 let failureCount = 0;
+let betSuccess = 0;
+let betFailures = {};
+let tokenBalances = {};
+
+// EXEC LOOP
+// loop().then()
 
 function getElapsedTimeInSeconds() {
     const currentTime = Date.now();
     return ((currentTime - startTime) / 1000).toFixed(2);
+}
+
+async function loop() {
+    console.log('🚀 Start')
+    while (true) {
+        await startLoopCycle();
+        // log shit
+    }
+}
+
+async function startLoopCycle() {
+    for (const token of userTokens) {
+        const {bearer, refresh} = token
+        await missions(bearer, refresh) // Ads compresa
+        await checkin(bearer, refresh)
+        await cinema(bearer, refresh)
+        await sumBalance(bearer, refresh) // senza bet disonesta
+        console.log(`📊 Token: ${bearer.slice(-5)} | Success: ${successCount} | Error: ${errorCount}`)
+        await pauseExecution(500)
+    }
+}
+
+async function makeBetRequest(bearerToken) {
+    const options = {
+        method: 'POST',
+        url: execute_bet_balance_api_url,
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Origin': 'https://dev.goatsbot.xyz',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://dev.goatsbot.xyz/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+        },
+        body: JSON.stringify(betData),
+        timeout: 5000
+    };
+    try {
+        const response = await cloudscraper(options);
+        betSuccess++;
+        return JSON.parse(response);
+    } catch (error) {
+        betFailures[bearerToken]++;
+        if (betFailures[bearerToken] % 25 === 0) {
+            console.log(`Errore bet: ${(error.message).slice(0, 4)} (totale errori: ${betFailures[bearerToken]}) --- Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
+        }
+        return null;
+    }
 }
 
 async function makeRequest(bearerToken) {
@@ -24,7 +121,21 @@ async function makeRequest(bearerToken) {
         url: 'https://dev-api.goatsbot.xyz/missions/action/66db47e2ff88e4527783327e',
         headers: {
             'Authorization': `Bearer ${bearerToken}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Origin': 'https://dev.goatsbot.xyz',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://dev.goatsbot.xyz/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
         }
     };
 
@@ -34,7 +145,7 @@ async function makeRequest(bearerToken) {
         successCount++;
         return jsonResponse;
     } catch (error) {
-        console.log(`Errore richiesta: ${(error.message).slice(0, 5)}`)
+        console.log(`Errore richiesta: ${(error.message).slice(0, 4)} --- Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
         failureCount++;
         return null;
     }
@@ -44,30 +155,42 @@ async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function logStatistics(token, response) {
-    if (response && token === myBearer) {
-        const elapsedTime = getElapsedTimeInSeconds();
-        const gained = (successCount * 200) + (bigMissSuccess * 1000);
-        console.log(`Tempo dall'avvio: ${elapsedTime} secondi (${(elapsedTime/60).toFixed(0)}) minuti 
-        Richieste elaborate: ${successCount} --- Richieste fallite: ${failureCount} --- Richieste misisoni speciali elaborate: ${bigMissSuccess}
-        Guadagno: ${gained.toFixed(0)} GOATS --- Missioni in esecuzione su ${bearerTokens.length} bearers:`)
-        for (const bearerToken of bearerTokens) {
-            console.log(`-${bearerTokens.indexOf(bearerToken)}) ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`)
-        }
-    } 
+function logStatistics() {
+    const elapsedTime = getElapsedTimeInSeconds();
+    const missedGain = failureCount * 200;
+    const gained = (successCount * 200) + (bigMissSuccess * 1000);
+    const cumulativeBalance = Object.values(tokenBalances).reduce((sum, value) => sum + value, 0);
+    console.log(`Tempo dall'avvio: ${Math.floor(elapsedTime/3600)} ore ${((elapsedTime/60) % 60).toFixed(0)} minuti ${(elapsedTime % 60).toFixed(0)} secondi`);
+    console.log(`-> Richieste elaborate: ${successCount} --- Richieste fallite: ${failureCount} --- Richieste missioni speciali elaborate: ${bigMissSuccess}`);
+    console.log(`-> Guadagno sessione: ${gained.toFixed(0)} GOATS --- Guadagno mancato: ${missedGain} GOATS --- Guadagno bets ${(betSuccess*0.22).toFixed(0)} GOATS`);
+    console.log(`-> Missioni in esecuzione su ${bearerTokens.length} bearers:`);
+    for (const bearerToken of bearerTokens) {
+        console.log(`-> (${bearerTokens.indexOf(bearerToken) + 1}): ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)} -> balance: ${tokenBalances[bearerToken]} GOATS`)
+    }
+    console.log(`-> Balance totale: ${cumulativeBalance} GOATS`);
 }
 
 async function performRequestCycle(bearerToken) {
-    const intervalId = setInterval(async () => {
-        const response = await makeRequest(bearerToken)
-        if (response) {
-            logStatistics(bearerToken, response);
+    setInterval(async () => {
+        await makeRequest(bearerToken);
+        await sleep(7500);
+        const betResponse = await makeBetRequest(bearerToken);
+        if (betResponse) {
+            tokenBalances[bearerToken] = +betResponse?.user?.balance;
+        }
+        if (bearerTokens.indexOf(bearerToken) === bearerTokens.length - 1) {
+            logStatistics();
         }
     }, 60500);
 }
 
 function start() {
     bearerTokens.forEach(async (bearerToken) => {
+        betFailures[bearerToken] = 0;
+        tokenBalances[bearerToken] = 0;
+        if (bearerTokens.indexOf(bearerToken) !== 0) {
+            await sleep(4000 * bearerTokens.indexOf(bearerToken));
+        }
         await performRequestCycle(bearerToken); 
     });
 }
@@ -75,10 +198,24 @@ function start() {
 async function getMissions(bearerToken) {
     const options = {
         method: 'GET',
-        url: 'https://api-mission.goatsbot.xyz/missions/user',
+        url: get_mission_api_url,
         headers: {
             'Authorization': `Bearer ${bearerToken}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Origin': 'https://dev.goatsbot.xyz',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://dev.goatsbot.xyz/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
         },
     };
 
@@ -86,7 +223,7 @@ async function getMissions(bearerToken) {
         const response = await cloudscraper(options);
         return JSON.parse(response);
     } catch (error) {
-        console.error(`Errore nel recuperare le missioni per Bearer Token: ${bearerToken}`, error.message);
+        console.error(`Errore (${(error.message)}) nel recuperare le missioni per Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
         return null;
     }
 }
@@ -94,20 +231,32 @@ async function getMissions(bearerToken) {
 async function executeMission(bearerToken, missionId) {
     const options = {
         method: 'POST',
-        url: `https://dev-api.goatsbot.xyz/missions/action/${missionId}`,
+        url: `${execute_mission_api_url}${missionId}`,
         headers: {
             'Authorization': `Bearer ${bearerToken}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Origin': 'https://dev.goatsbot.xyz',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://dev.goatsbot.xyz/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
         }
     };
-
     try {
         const response = await cloudscraper(options);
         console.log(`Missione ${missionId} completata con successo.`);
-
         return JSON.parse(response);
     } catch (error) {
-        console.error(`Errore nell'eseguire la missione ${missionId} per Bearer Token: ${bearerToken}`, error.message);
+        console.error(`Errore (${(error.message).slice(0, 4)}) nell'eseguire la missione ${missionId} per Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
     }
 }
 
@@ -117,6 +266,7 @@ async function processMissionsForBearer(bearerToken) {
     for (const [missionGroup, missions] of Object.entries(missionsData)) {
         for (const mission of missions) {
             if (mission.status === false) {
+                await sleep(250);
                 await executeMission(bearerToken, mission._id);
                 bigMissSuccess++;
             } else {
@@ -126,23 +276,29 @@ async function processMissionsForBearer(bearerToken) {
     }
 }
 
-async function processAllBearers() {
+async function startHourlyProcess() {
     for (const bearerToken of bearerTokens) {
+        if (bearerTokens.indexOf(bearerToken) !== 0) {
+            await sleep(2500 * bearerTokens.indexOf(bearerToken));
+        }
         console.log(`Controllo missioni da eseguire per Bearer Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
         await processMissionsForBearer(bearerToken);
     }
-}
-
-function startHourlyProcess() {
-    processAllBearers();
-    setInterval(processAllBearers, 60 * 60 * 1000); // Ripeti ogni ora
+    setInterval(async () => {
+        for (const bearerToken of bearerTokens) {
+            if (bearerTokens.indexOf(bearerToken) !== 0) {
+                await sleep(5000 * bearerTokens.indexOf(bearerToken));
+            }
+            console.log(`Controllo missioni da eseguire per Bearer Token: ${bearerToken.slice(0, 5)}...${bearerToken.slice(-5)}`);
+            await processMissionsForBearer(bearerToken);
+        }
+    }, 60 * 60 * 1000 * 8); // Ripeti ogni 8 ore
 }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Service is running on port ${port}`);
 });
-
 startHourlyProcess();
 start();
 
